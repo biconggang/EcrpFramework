d9c6836ffa40ea52bc8e213acf1425bd41727536
biconggang <7130255@qq.com>
2022-03-02 10:32:08 +0800

    编译后可正常运行2

---
+++ src/main/java/cn/org/ecrp/ui/control/DoubleSpinner.java
@@ -0,0 +1,47 @@
+package cn.org.ecrp.ui.control;
+
+import javafx.beans.NamedArg;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+
+/**
+ * description <br>
+ *
+ * @author YourName
+ * @date 2021-12-16 15:39
+ */
+public class DoubleSpinner extends Spinner<Double> {
+
+    public DoubleSpinner(
+            @NamedArg(value = "min", defaultValue = "0") double min,
+            @NamedArg(value = "max", defaultValue = "100") double max,
+            @NamedArg(value = "initialValue", defaultValue = "0") double initialValue,
+            @NamedArg(value = "amountToStepBy", defaultValue = "1") double amountToStepBy) {
+        super(min, max, initialValue, amountToStepBy);
+    }
+
+    /**
+     * 赋值，当 value 超出最大最小值范围时抛出异常
+     */
+    public void setValue(double value) {
+        SpinnerValueFactory.DoubleSpinnerValueFactory valueFactory = getSpinnerValueFactory();
+        if (value < valueFactory.getMin() || value > valueFactory.getMax()) {
+            throw new IllegalArgumentException(
+                    "Value out of bounds(" + valueFactory.getMin() + "~" + valueFactory.getMax() + ")."
+            );
+        }
+        valueFactory.setValue(value);
+    }
+
+    public SpinnerValueFactory.DoubleSpinnerValueFactory getSpinnerValueFactory() {
+        return (SpinnerValueFactory.DoubleSpinnerValueFactory) getValueFactory();
+    }
+
+    public double getMin() {
+        return getSpinnerValueFactory().getMin();
+    }
+
+    public double getMax() {
+        return getSpinnerValueFactory().getMax();
+    }
+}
---
+++ src/main/java/cn/org/ecrp/ui/helper/MenuHelper.java
@@ -0,0 +1,43 @@
+package cn.org.ecrp.ui.helper;
+
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.image.ImageView;
+
+import java.util.function.Supplier;
+
+/**
+ * description <br>
+ *
+ * @author bicg
+ * @date 2021-12-16 16:20
+ */
+public class MenuHelper {
+
+    public static ContextMenu contextMenu(MenuItem... menuItems) {
+        return new ContextMenu(menuItems);
+    }
+
+    public static MenuItem menuItem(String text, Runnable action) {
+        return menuItem(text, (Supplier<ImageView>) null, action);
+    }
+
+    public static MenuItem menuItem(String text, ImageView icon, Runnable action) {
+        return menuItem(text, () -> icon, action);
+    }
+
+    public static MenuItem menuItem(String text, Supplier<ImageView> iconSupplier, Runnable action) {
+        final MenuItem menuItem = new MenuItem(text);
+        menuItem.setOnAction(event -> action.run());
+        if (iconSupplier != null) {
+            menuItem.setGraphic(iconSupplier.get());
+        }
+        return menuItem;
+    }
+
+    public static SeparatorMenuItem separator() {
+        return new SeparatorMenuItem();
+    }
+
+}
